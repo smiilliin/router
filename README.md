d84fa940957f651ca1ee80ccafa358bc562e04ba
smiilliin <smiilliindeveloper@gmail.com>
2022-12-30 09:36:57 +0900

    First commit

---
+++ README.md
@@ -0,0 +1,35 @@
+# Router
+
+내부 서버들(포트)을 프록시로 연결시켜주는 앱입니다!
+
+## 사용법
+
+src/bind.json
+
+```json
+{
+  "(/이름)": 12345 //(포트)
+}
+//이름이 index인것은 "/" 경로 포트에요!
+```
+
+.env
+
+```
+CERT_PATH=(인증서 cert 경로)
+KEY_PATH=(인증서 key 경로)
+```
+
+설치 및 실행
+
+```bash
+npm install
+
+./run.sh
+```
+
+죽이기
+
+```bash
+./kill.sh
+```
---
+++ src/index.ts
@@ -0,0 +1,72 @@
+import express from "express";
+import httpproxy from "http-proxy";
+import fs from "fs";
+import dotenv from "dotenv";
+import https from "https";
+
+dotenv.config();
+
+const app = express();
+
+const httpProxy = httpproxy.createProxyServer();
+
+const error500 = (res: express.Response) => {
+  return () => {
+    res.status(500).send(fs.readFileSync("src/error500.html").toString());
+  };
+};
+const error404 = (res: express.Response) => {
+  return () => {
+    res.status(404).send(fs.readFileSync("src/error404.html").toString());
+  };
+};
+
+const bind = (name: string, port: number) => {
+  app.use(`/${name}`, (req, res) => {
+    httpProxy.web(
+      req,
+      res,
+      { target: `http://localhost:${port}` },
+      error500(res)
+    );
+  });
+};
+const bindIndex = (port: number) => {
+  app.get(`/`, (req, res) => {
+    httpProxy.web(
+      req,
+      res,
+      { target: `http://localhost:${port}` },
+      error500(res)
+    );
+  });
+};
+
+const binds = JSON.parse(fs.readFileSync("src/bind.json").toString());
+
+Object.keys(binds).forEach((name: string) => {
+  const port = binds[name] as number;
+
+  console.log(`${name} ${port}`);
+  if (name !== "index") {
+    bind(name, port);
+  } else {
+    bindIndex(port);
+  }
+});
+
+app.get("*", (req, res) => {
+  error404(res)();
+});
+
+const httpsServer = https.createServer(
+  {
+    key: fs.readFileSync(process.env["KEY_PATH"] as string).toString(),
+    cert: fs.readFileSync(process.env["CERT_PATH"] as string).toString(),
+  },
+  app
+);
+
+httpsServer.listen(443, () => {
+  console.log("Running gateway on port 443");
+});
