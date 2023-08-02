a32c25255d7635502db7c0ce44b6bc05e8f01286
smiilliin <smiilliindeveloper@gmail.com>
2023-08-02 14:18:38 +0900

    allow all origin

--- src/index.ts
+++ src/index.ts
@@ -25,6 +25,7 @@ app.use((req, res, next) => {
     res.redirect(`https://${env.host}${req.originalUrl}`);
   } else {
     res.header("Strict-Transport-Security", "max-age=63072000; includeSubDomains; preload");
+    res.header("Access-Control-Allow-Origin", "*");
     next();
   }
 });
